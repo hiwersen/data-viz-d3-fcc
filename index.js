018c8c889e6cf64d26da6c6d5beab8f1889161fb
Hiwersen Angelo Gnocchi Godoy <100746220+hiwersen@users.noreply.github.com>
2023-06-12 14:54:47 +0100

    feat: Implement express server

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const express = require('expres');
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use("/", (req, res, next) => {
+    console.log(req.path);
+    next();
+})
+
+app.use('/', express.static(__dirname + '/public'));
+
+const listener = app.listen(port, () => {
+    console.log(`Listening on port ${listener.address().port}`);
+});

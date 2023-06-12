da5375538be3949a0ac7238086826731fa20cc68
Hiwersen Angelo Gnocchi Godoy <100746220+hiwersen@users.noreply.github.com>
2023-06-12 14:56:56 +0100

    fix: Delete  console.log get req path middleware

--- index.js
+++ index.js
@@ -3,11 +3,6 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use("/", (req, res, next) => {
-    console.log(req.path);
-    next();
-})
-
 app.use('/', express.static(__dirname + '/public'));
 
 const listener = app.listen(port, () => {
